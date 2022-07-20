daed1244dd5a79d0113a460fd6ff497c7082d70a
Mitch <mjoerussell@gmail.com>
2022-07-19 20:04:03 -0500

    Add src/client.zig to the external API of tortie

--- src/lib.zig
+++ src/lib.zig
@@ -2,4 +2,5 @@ pub const TortieServer = @import("TortieServer.zig");
 pub const TortieClient = @import("TortieClient.zig");
 pub const EventLoop = @import("event_loop.zig").EventLoop;
 pub const FileSource = @import("FileSource.zig");
+pub const Client = @import("client.zig").Client;
 pub const http = @import("http");
